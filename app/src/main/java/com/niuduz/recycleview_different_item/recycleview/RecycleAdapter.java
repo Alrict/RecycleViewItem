743a6d3968a4da0c232f9d8c88cb6cc30477a0e2
alrict <353296717@qq.com>
2018-04-27 17:22:56 +0800

    1.修改个人信息
    Signed-off-by: alrict <353296717@qq.com>

--- app/src/main/java/com/niuduz/recycleview_different_item/recycleview/RecycleAdapter.java
+++ app/src/main/java/com/niuduz/recycleview_different_item/recycleview/RecycleAdapter.java
@@ -139,6 +139,7 @@ public class RecycleAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder
 
         public ViewHolderC(View itemView) {
             super(itemView);
+
             text = (TextView) itemView.findViewById(R.id.text);
         }
     }
